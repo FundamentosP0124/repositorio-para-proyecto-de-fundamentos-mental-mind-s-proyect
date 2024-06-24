5de5f52fb2926f957a68925a0d026338bb87498b
Cesardelao123 <00205824@uca.edu.sv>
2024-06-24 12:33:55 -0600

    Cuerpo base para clonar y crear las ramas del proyecto

---
+++ Trivia.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+/*Juego de trivia de preguntas al azar con 3 niveles de dificultad y un maximo de 3 equivocaciones
+de acuerdo a la modalidad seleccionada del juego sin es por limites de inaciertos puntuacion final
+segun si es el modo multijugador o de manera individual*/
+
+int main(void)
+{
+
+    return 0;
+}
+
+void menu()
+{
+
+    int opcion;
+
+    cout << endl;
+    cout << "Presione enter para empezar";
+    cin.get(); // En espera del enter para seguir
+
+    cout << endl;
+    cout << "////MENU////" << endl;
+    /*Agregar el resto de opciones en el menu*/
+
+    cout << "0? - TERMINAR JUEGO";
+
+    switch (opcion)
+    {
+    case 4:
+        exit(0);
+        break;
+
+    default:
+        cout << "Opcion inexistente";
+        break;
+    }
+}
+
+int cantidadJugadores(int Cant)
+{
+    
+}
